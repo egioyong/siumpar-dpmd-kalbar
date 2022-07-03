710c9164b45a8c04dcf4af45291427552e6bbfb2
egioyong <egioyong15@gmail.com>
2022-07-03 15:25:49 +0700

    hapus operator

---
+++ suratmasuk/hapus.php
@@ -0,0 +1,23 @@
+<?php 
+include "../koneksi.php";
+
+if(isset($_GET['id'])){
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM suratmasuk WHERE id_suratmasuk = $id";
+$query = mysqli_query($db,$sql);
+
+    if($query){
+        echo"
+        <script>
+            alert ('Data Berhasil Dihapus!');
+            document.location.href = '../suratmasuk.php';
+        </script>
+       ";
+    }
+
+
+}
+
+?>
